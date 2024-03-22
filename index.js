4f9547cb2ad4d34bcd029454afbc75fed4c3061e
Kitui Kelly <kituikelly@gmail.com>
2024-03-22 18:12:12 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+
+}
+
+/* setBestCustomer(): Write a function that
+ when called, declares a variable called bestCustomer in 
+ global scope and assigns it to be 'not bob'. (Poor Bob.) Also, poor us, d
+ eclaring a global variable from inside a function is one of those things we would never 
+want to do normally, but is good for us to explore right now. */
+
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = bestCustomer = 'not bob';
+    return bestCustomer;
+    };
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+} 
+
+const leastFavoriteCustomer = "";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "KEllY"
+}
